5a0fc10ad19f42914670332737fa50c43a73a42c
Analuvazz <analuvaz1997@gmail.com>
2024-04-26 08:05:24 -0300

    subir aulas

--- server1.js
+++ server1.js
@@ -1,13 +1,13 @@
 // 19/04 - criar o primeiro servidor web usando node
-const http =require('http');
+const http = require('http');
 
-const server =http.createServer((req,res)=>{
+const server = http.createServer((req,res)=>{
     console.log("Efetuando pedido!\n");
     //declarando um header
     res.setHeader('content-type','text/html');
     res.write("<h1>O que vossa senhoria deseja?</h1>");
-    res.write('<h2>Dormir</h2>');
-    res.write('<h3>Cafezinho</h3>');
+    res.write("<h2>Dormir</h2>");
+    res.write("<h3>Cafezinho</h3>");
     res.end();
 })
 
--- sever2.js
+++ sever2.js
@@ -11,8 +11,8 @@ const server=http.createServer((req,res)=>{
     res.setHeader('content-type','text/html');
     fs.readFile('page1.html',(erro,data)=>{
         if(erro){
-            res.write("Deu errado,sinto muito")
-            res.end()
+            res.write("Deu errado, sinto muito.");
+            res.end();
         }else{
             res.write(data);
             res.end();
